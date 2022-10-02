10e7a057347931afa384d5e73d5b2ef9500dce22
junghye01 <tweetiijh@naver.com>
2022-10-02 18:36:06 +0900

    2022-10-02 실습과제#5 5번

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 name=input('이름을 입력하세요: ')
 college_class=input('학번을 입력하세요: ')
 major=input('학과를 입력하세요: ')
-
+grade=input('학년을 입력하세요: ')
 
 print('\<출력>')
 print('이름:',name)
 print('학번:',college_class)
 print('학과:',major)
+print('학년:',grade)
