7c992efadfb93b6c02e58a5b7febe5e3e3a2fbfd
junghye01 <tweetiijh@naver.com>
2022-10-02 18:41:07 +0900

    2022-10-02 실습과제#5 7번

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 name=input('이름을 입력하세요: ')
 college_class=input('학번을 입력하세요: ')
 major=input('학과를 입력하세요: ')
-<<<<<<< HEAD
-grade=input('학년을 입력하세요: ')
-=======
 school=input('학교를 입력하세요: ')
->>>>>>> 3131ff90df19e5d8675c8059a83fd5ce08366ce3
+grade=input('학년을 입력하세요: ')
 
 print('\<출력>')
 print('이름:',name)
 print('학번:',college_class)
 print('학과:',major)
-<<<<<<< HEAD
-print('학년:',grade)
-=======
 print('학교:',school)
->>>>>>> 3131ff90df19e5d8675c8059a83fd5ce08366ce3
+print('학년:',grade)
